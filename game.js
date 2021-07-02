ac38dcc1661e8b47a8fe10bc40f4bdb854365afe
Shivam-106 <69721189+Shivam-106@users.noreply.github.com>
2021-07-02 23:58:44 +0530

    draw image to canvas

--- game.js
+++ game.js
@@ -5,10 +5,50 @@ const ctx = cvs.getContext("2d");
 // GAME VARS AND CONSTS
 let frames = 0;
 
+// LOAD SPRITE IMAGE
+const sprite = new Image();
+sprite.src = "img/sprite.png";
+
+// BACKGROUND
+const bg = {
+    sX : 0,
+    sY : 0,
+    w : 275,
+    h : 226,
+    x : 0,
+    y : cvs.height - 226,
+
+    draw : function() {
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+    }
+}
+
+// FOREGROUND
+const fg = {
+    sX: 276,
+    sY: 0,
+    w: 224,
+    h: 112,
+    x: 0,
+    y: cvs.height - 112,
+    
+    dx : 2,
+    
+    draw : function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+    }
+}
+
+// BIRD
+
+
 // DRAW
 function draw() {
     ctx.fillStyle = "#70c5ce";
     ctx.fillRect(0, 0, cvs.width, cvs.height);
+
+    bg.draw();
+    fg.draw();
 }
 
 // UPDATE
