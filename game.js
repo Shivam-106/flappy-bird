6d023175ee549193b54275a7c417d57f71431140
Shivam-106 <69721189+Shivam-106@users.noreply.github.com>
2021-07-09 03:35:44 +0530

    gravity and flap

--- game.js
+++ game.js
@@ -79,6 +79,10 @@ const bird = {
 
     frame : 0,
     
+    gravity : 0.25,
+    jump : 4.6,
+    speed : 0,
+
     draw : function(){
         let bird = this.animation[this.frame];
 
@@ -86,7 +90,7 @@ const bird = {
     },
 
     flap : function(){
-        
+        this.speed = - this.jump;
     },
 
     update: function(){
@@ -97,6 +101,21 @@ const bird = {
         // FRAME GOES FROM 0 To 4, THEN AGAIN TO 0
         this.frame = this.frame%this.animation.length;
         
+        if(state.current == state.getReady){
+            this.y = 150; // RESET POSITION OF THE BIRD AFTER GAME OVER
+        }else{
+            this.speed += this.gravity;
+            this.y += this.speed;
+            
+            if(this.y + this.h/2 >= cvs.height - fg.h){
+                this.y = cvs.height - fg.h - this.h/2;
+                if(state.current == state.game){
+                    state.current = state.over;
+                }
+            }
+
+        }
+
     }
 
 }
