0f0701ce2a9a1fb7097ca9bdfebde1be597cccc5
Shivam-106 <69721189+Shivam-106@users.noreply.github.com>
2021-07-09 02:47:44 +0530

    implement game state

--- game.js
+++ game.js
@@ -9,6 +9,29 @@ let frames = 0;
 const sprite = new Image();
 sprite.src = "img/sprite.png";
 
+// GAME STATE
+const state = {
+    current : 0,
+    getReady : 0,
+    game : 1,
+    over : 2
+}
+
+// CONTROL THE GAME
+cvs.addEventListener("click", function(evt){
+    switch(state.current){
+        case state.getReady:
+            state.current = state.game;
+            break;
+        case state.game:
+            bird.flap();
+            break;
+        case state.over:
+            state.current = state.getReady;
+            break;
+    }
+});
+
 // BACKGROUND
 const bg = {
     sX : 0,
@@ -60,7 +83,12 @@ const bird = {
         let bird = this.animation[this.frame];
 
         ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h, this.x - this.w/2, this.y - this.h/2, this.w, this.h);
-    }
+    },
+
+    flap : function(){
+        
+    },
+
 }
 
 // GET READY MESSAGE
@@ -73,7 +101,9 @@ const getReady = {
     y : 80,
     
     draw: function(){
+        if(state.current == state.getReady){
             ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        }
     }
     
 }
@@ -88,7 +118,9 @@ const gameOver = {
     y : 90,
     
     draw: function(){
-            ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        if(state.current == state.over){
+            ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);   
+        }
     }
     
 }
